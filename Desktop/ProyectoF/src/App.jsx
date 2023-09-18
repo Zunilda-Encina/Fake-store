02006214f612723cc7e2d63a756bed159d9ad81e
Zunilda <zunilda.encina02@gmail.com>
2023-09-17 22:22:11 -0300

    Subo paguinacion

--- Desktop/ProyectoF/src/App.jsx
+++ Desktop/ProyectoF/src/App.jsx
@@ -5,20 +5,37 @@ import './app.css'
 function App() {
 
 const [productos, setProductos] = useState ([])
-
+const [index, setIndex] = useState(1)
 useEffect (() => {
 
-  fetch('https://fakestoreapi.com/products')
+  fetch(`https://fakestoreapi.com/products/${index}`)
   .then((response) => (response).json())
-  .then((data) => setProductos(data))
+  .then((data) => {
 
-
+    if (Object.keys(data).length === 0) {
+      setProductos([]);
+    } else {
+      setProductos([data]);
+    }
+  })
   .catch((error) => console.error(error))
-}, [])
+}, [index])
 
+const handlePrev = () => {
+  if (index > 1) {
+    setIndex(index - 1);
+  }
+};
+
+const handleNext = () => {
+  if (index < 40) {
+    setIndex(index + 1);
+  }
+};
   return (
     <>  
-        <header>NAV</header>
+        <header> nav
+        </header>
         <main>
         {
         productos.length > 0 ?
@@ -27,12 +44,15 @@ useEffect (() => {
         price={producto.price}
         image={producto.image}
         category={producto.category}
-   //     description={producto.description} 
+       description={producto.description} 
    />)
           : <p> No hay resultado para su busqueda</p>
       }
         </main>
-
+     <div>
+      <button disabled={index <= 1} onClick={handlePrev}>◀</button>
+      <button disabled={index >= 40} onClick={handleNext}>▶</button>
+     </div>
     </>
   )
 }
--- Desktop/ProyectoF/src/components/productos.jsx
+++ Desktop/ProyectoF/src/components/productos.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './productos.css'
 
 const info = ({title, price, category, description, image}) =>{
+  const [Description, setDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setDescription(!Description);
+  };
+  const hideDescription = () => {
+    setDescription(false);
+  };
      {
         return (
           <div className='card'>
-                <img className='card_img' src={image} alt='Imagenes de los productos'/>
+                <img className='card_img' src={image} alt='{title}'/>
               <div className='card_data'>
                 <h2 className='box'>{title}</h2>
                 <h3 className='box'>{price}</h3>
                 <h2 className='box'>{category}</h2>
-                <p className='box'>{description}</p>
+                {Description ? ( <><p>{description}</p>
+            <button onClick={hideDescription}>Ver menos</button>  </> ) : 
+            ( <button onClick={toggleDescription}>Más Info</button> )}
               </div>
           </div>
         )
